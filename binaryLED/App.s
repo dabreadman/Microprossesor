c874935c301a866454d331c86fc4610219273b96
dabreadman <43485369+dabreadman@users.noreply.github.com>
2020-02-07 17:55:34 +0000

    Implemented all remaining codes.

--- binaryLED/App.s
+++ binaryLED/App.s
@@ -6,7 +6,6 @@
 
 	EXPORT	start
 start
-
 IO1DIR	EQU	0xE0028018
 IO1SET	EQU	0xE0028014
 IO1CLR	EQU	0xE002801C
@@ -16,8 +15,69 @@ IO1CLR	EQU	0xE002801C
 ;   get binary representation using TABLE
 ; 	calls flash subroutine
 	
-	mov r0,#3
-	BL flash
+	ldr r0, =0x419
+	mov r1, r0 ; creates a copy
+	ldr r4, =0x40000000	; address
+	add r6,r4,#10		; sign address
+	ldr r5, =0
+	strb r5,[r6] 		; clears memory
+	
+sign movs r1,r1, lsl #1
+	 ldr r2,=0
+	 adc r2,r2,#0
+	 cmp r2,#1
+	 bne startdiv
+	 ldr r2, =1		; stores neg bit
+	 strb r2,[r6]
+	 ldr r2,=0xffffffff
+	 eor r0,r0,r2	; flip all bits 
+	 add r0,r0,#1 	; +1
+	
+	
+startdiv	ldr r2,=DIVTABLE 
+			ldr r8,=0
+div		cmp r2,r6			; if still in bounds
+		bge leds
+		ldr r5,[R2],#4 ; largers number of power's of 10
+		cmp r0,#0			; if still have remaining
+		beq leds
+		
+div2 	cmp r0,r5			; if number larger than divisor
+		blt small
+		sub r0,r0,r5		; numb = numb - divisor
+		add r8,r8,#1		; increment count
+		b div2
+	 
+small strb r8,[r4],#1
+	  ldr r8,=0			; reset count
+	  b div
+	 
+leds	ldr r4, =0x40000000	; address
+fstart	cmp r4,r6			; if the relative value of the count is larger than 0
+		bgt	nothing
+		ldrb r5,[r4],#1		; find the count and increment
+		cmp r5,#0			; if nothing
+		bgt startled
+		b fstart
+
+nothing ldr r0, =0		;nothing
+		bl flash	
+		b stop
+
+startled	sub r4,r4,#1 	; return to correct address
+			ldrb r5,[r6]		; loads the sign
+			cmp r5,#1		; if negative
+			bne flashnum
+			ldr r0, =0xB		; flash negative sign
+			BL flash
+		
+flashnum	cmp r4,r6
+			bge stop
+			ldrb r0,[r4],#1	; get count and increment address
+			bl flash
+			b flashnum
+			
+stop	B	stop
 
 ;
 ;	flash subroutine
@@ -27,7 +87,7 @@ IO1CLR	EQU	0xE002801C
 ;	output:
 ;	 none
 
-flash   PUSH {r3-r6,pc}
+flash   PUSH {r3-r6,lr}
 		ldr	r1,=IO1DIR
 		ldr	r2,=0x000f0000	;select P1.19--P1.16
 		str	r2,[r1]		;make them outputs
@@ -38,11 +98,14 @@ flash   PUSH {r3-r6,pc}
 ; r2 points to the CLEAR register
 
 	
-	ldr	r5,=0x000170000	; end when the mask reaches this value
-	ldr r6,=0
-		
-		ldr	r3,=0x000080000	; start with P1.16.
-floop	cmp r0,#0
+	ldr	r5,=0x000080000	; end when the mask reaches this value
+
+		cmp r0,#0
+		bne fs 
+		ldr r0,=0xf
+fs		ldr	r3,=0x000080000	; start with P1.16.
+floop	ldr r6,=0
+		cmp r0,#0
 		beq ds 
 		movs r0,r0,lsr #1
 		adc r6,r6,#0
@@ -53,23 +116,18 @@ nset	mov r3,r3,lsr #1
 		b floop
 
 ;delay for about a half second
-ds	ldr	r4,=4000000
+ds	ldr	r4,=8000000
 dloop	subs	r4,r4,#1
 	bne	dloop
 	
-offs	str	r3,[r1]		;set the bit -> turn off the LED
-		mov	r3,r3,lsl #1	;shift up to next bit. P1.16 -> P1.17 etc.
+offs	mov	r3,r3,lsl #1	;shift up to next bit. P1.16 -> P1.17 etc.
+		str	r3,[r1]		;set the bit -> turn off the LED
 		cmp	r3,r5
 		blt	offs
-		
-		POP {r3-r6,lr}
-stop	B	stop
-
-TABLE   DCB 0xF, 0x1, 0x2, 0x3 ; 0-3
-		DCB 0x4, 0x5, 0x6, 0x7 ; 4-7
-		DCB 0x8, 0x9  		   ; 8-9
-		DCB 0xA				   ; - sign
+		POP {r3-r6,pc}
 		
 
+
+DIVTABLE DCD 1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1	
 
 	END
